e66b1d9fb33778c38dd54b3ff391ea472de2fc95
Okkiy Metta <okkiy.metta@gmail.com>
2021-06-04 18:21:12 +0700

    Apply admin template using SB Admin

---
+++ app/javascript/packs/admin/admin.js
@@ -0,0 +1,47 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "jquery/src/jquery"
+import "popper.js/dist/popper"
+import "bootstrap/dist/js/bootstrap"
+import "@fortawesome/fontawesome-free/js/all"
+import "startbootstrap-sb-admin/src/js/scripts"
+
+window.addEventListener('turbolinks:load', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+// window.addEventListener('DOMContentLoaded', event => {
+//     // Simple-DataTables
+//     // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+//     const datatablesSimple = document.getElementById('datatablesSimple');
+//     if (datatablesSimple) {
+//         new simpleDatatables.DataTable(datatablesSimple);
+//     }
+// });
